7fa06880dc5c113086094e754003f20ea7ea2c56
Filip Szumski <filipszumski@gmail.com>
2021-08-02 21:05:01 +0200

    changed text - olowek

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - gumka
+          Learn React - olowek
         </a>
       </header>
     </div>
